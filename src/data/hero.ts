0cf66c1661d789038f3d26f4dac2f784eb213bc9
Black-randy <asiri.2704gg@gmail.com>
2025-06-11 02:25:02 +0530

    feat: ✨ add heroContent data object
    * Introduced a new `heroContent` object to store key information for the hero section.
    * Includes properties for `title`, `subtitle`, `description`, and `image`.

---
+++ src/data/hero.ts
@@ -0,0 +1,7 @@
+export const heroContent = {
+  title: 'Λsiri',
+  subtitle: 'Full Stack Developer',
+  description: `Passionate about crafting exceptional digital experiences, building scalable and user-friendly applications with modern web technologies.
+Experienced in full-stack development, committed to delivering excellent user experiences and high-performance technical solutions.`,
+  image: 'public/logo/Logo-A-Light.png' // Replace with the actual path or URL to your image
+};
