266f9019da7d4ff6c5b3026e5f74c7e5f89bb96b
Black-randy <59816942+Black-randy@users.noreply.github.com>
2025-07-11 23:11:12 +0530

    ✨ feat(footer): integrate Marquee component and update theme-based icon color

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,11 +1,16 @@
 import { Separator } from '@/components/ui/separator';
-
+import { useTheme } from '@/providers/theme-provider'; 
+import { Marquee } from "@/components/ui/marquee"
 export function Footer() {
+  const { theme } = useTheme(); 
   return (
+    <>
+    <Marquee fontSize="sm" text=" Follow me On Social Media To Stay Updated! " /> 
     <footer className="border-t">
       <div className="container mx-auto px-4 h-12 md:h-12 flex items-center justify-between text-sm text-muted-foreground mb-16 md:mb-0">
         <span>
-          © 2025 Asiri. Built with 💛 </span>
+          © 2025 Asiri. Built with {theme === 'dark' ? '🤍' : '🖤'}
+        </span>
         <Separator orientation="vertical" className="h-4" />
         <span>
           <span role="img" aria-label="React">⚛️</span> React &nbsp;
@@ -15,5 +20,6 @@ export function Footer() {
         </span>
       </div>
     </footer>
+    </>
   );
 }
